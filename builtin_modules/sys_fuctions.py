761a5133a5e6456670bc85e472446d661c41265f
can kurt <can.kurt.aa@gmail.com>
2020-06-25 01:29:43 +0300

    sys stuff added

---
+++ builtin_modules/sys_fuctions.py
@@ -0,0 +1,14 @@
+import sys
+
+# print to console
+sys.stdout.write("hi")
+
+# write error message to console
+sys.stderr.write("sa")
+sys.stderr.flush()
+
+# get programs parameters
+sys.argv
+
+# exit program
+sys.exit()
