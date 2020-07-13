4e80d9c3761b2a5d19313df91389443d72b3687a
can kurt <can.kurt.aa@gmail.com>
2020-07-13 04:00:28 +0300

    hard copy a list

--- 05-data_structures/lists.py
+++ 05-data_structures/lists.py
@@ -4,9 +4,18 @@ items2 = [1,2,3,1,1,4,2,1]
 # reverse list
 items[::-1]
 
+
+# hard copy a list
+a = [1,2,3]
+b = a # shallow copy
+b = a[:] # hard copy
+
+
 # most_frequent_item
 most_frequent_item = max(set(items),key=items.count)
 
+# remove duplicates
+list(set(items))
 
 # count elements of a list
 from collections import Counter
@@ -14,7 +23,4 @@ Counter(items)
 
 # make the list immutable
 items2 = frozenset(items2)
-
-# remove duplicates
-list(set(items))
 
