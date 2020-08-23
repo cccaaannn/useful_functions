8d421a20a262bcc4c4ac6ccdf310fcf3b922bb0d
can kurt <can.kurt.aa@gmail.com>
2020-08-23 03:38:47 +0300

    more on string formatting

--- 05-data_structures/strings.py
+++ 05-data_structures/strings.py
@@ -39,3 +39,26 @@ print(f'name is {name} value is {value}')
 
 inner = f"fsting {value}"
 print(f'you can use {f"nested {inner}"}')
+
+
+# float formatting
+
+float_number = 4863.4343091  
+
+# round ing with a precision
+print(f"{float_number:.6}")
+print("{0:.6}".format(float_number))
+
+# cutting from a point
+print("{0:.4f}".format(float_number))
+
+# adding seperators to numbers
+print("{0:,.4f}".format(float_number))
+print("{0:_.4f}".format(float_number))
+
+
+# converting to percentage automatically
+questions = 50
+correct_answers = 10
+print(f"{correct_answers / questions :.2%}")
+
