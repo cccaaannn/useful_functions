ee45b98e940ddefcc086a97370fcfbd1ec12a8ca
can kurt <can.kurt.aa@gmail.com>
2020-09-03 01:08:19 +0300

    dirs changed

---
+++ 03-other_examples/print_examples.py
@@ -0,0 +1,8 @@
+
+print("example", "second value", sep='00000', end='\n\n\n')
+
+print("example", "second value", sep='0', end='')
+
+# print to a file
+# with open('example.txt', 'w') as f:
+#     print("example", file = f)
