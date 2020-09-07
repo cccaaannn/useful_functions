a0d1e309972ce9b3ad1b4eab27242256dc289e16
can kurt <can.kurt.aa@gmail.com>
2020-09-08 00:08:52 +0300

    deleted files

--- 02-advanced_functions/print_examples.py
+++
@@ -1,8 +0,0 @@
-
-print("example", "second value", sep='00000', end='\n\n\n')
-
-print("example", "second value", sep='0', end='')
-
-# print to a file
-# with open('example.txt', 'w') as f:
-#     print("example", file = f)
