028a157401d3e05cf3fb42217b2506d7ba3e1349
can kurt <can.kurt.aa@gmail.com>
2020-09-20 17:46:01 +0300

    platform examples added

---
+++ 00-builtin_modules/platform_examples.py
@@ -0,0 +1,26 @@
+import platform
+
+
+# system
+print(platform.uname())
+print()
+
+# os
+print(platform.architecture())
+print(platform.system())
+print(platform.version())
+print()
+
+# hardware
+print(platform.machine())
+print(platform.processor())
+print()
+
+# python
+print(platform.python_build())
+print(platform.python_compiler())
+print(platform.python_branch())
+print(platform.python_implementation())
+print(platform.python_revision())
+print(platform.python_version())
+print(platform.python_version_tuple())
