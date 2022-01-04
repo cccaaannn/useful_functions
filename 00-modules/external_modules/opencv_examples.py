e69a3893d7a335790d2d646929b3ea103f5b81b1
cccaaannn <can.kurt.aa@gmail.com>
2022-01-04 17:51:16 +0300

    flip_images example added

--- 00-modules/external_modules/opencv_examples.py
+++ 00-modules/external_modules/opencv_examples.py
@@ -17,3 +17,19 @@ cv2.imshow('image', img)
 cv2.waitKey(0)
 
 
+
+def flip_images(source_dir, save_dir, flip_option=1):
+    """
+    flip_option=0 Vertical
+    flip_option=1 Horizontal
+    flip_option=-1 Both
+    """
+    import cv2
+    import os
+    for index, img in enumerate(os.listdir(source_dir)):
+        print(index)
+        originalImage = cv2.imread(os.path.join(source_dir, img))
+        flipHorizontal = cv2.flip(originalImage, flip_option)
+        cv2.imwrite(os.path.join(save_dir, img), flipHorizontal)
+
+
